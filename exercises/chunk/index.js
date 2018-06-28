cc332d55a3584cfa8b4baa4aeef513021dc6ca6a
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-06-28 15:54:45 -0700

    completed chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,16 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  var res = [];
+  var index = 0;
+
+  while (index < array.length) {
+    res.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return res;
+}
 
 module.exports = chunk;
