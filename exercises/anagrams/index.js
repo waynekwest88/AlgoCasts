4f038998bdaca17162dcad87b59c869aad12e094
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-06-28 16:16:55 -0700

    completed anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,36 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  return stringA.split('').sort().join('') === stringB.split('').sort().join('')
+}
 
 module.exports = anagrams;
+
+// function buildMap(string) {
+//   let map = {};
+
+//   for (var i = 0; i < string.length; i++) {
+//     var lowerCaseChar = string[i].toLowerCase();
+//     map[lowerCaseChar] = map[lowerCaseChar] + 1 || 1;
+//   }
+
+//   return map;
+// }
+
+// function anagrams(stringA, stringB) {
+//   var mapA = buildMap(stringA);
+//   var mapB = buildMap(stringB);
+
+//   if (stringA.length === stringB.length) {
+//     for (var char in mapA) {
+//       if (mapA[char] !== mapB[char]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
