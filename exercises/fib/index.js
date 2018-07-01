b484a6dec3a1687b64b607cf72ea638d79aa13a2
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-07-01 16:15:15 -0700

    completed fibonacci - included iterative, recursive, and memoized recursive solutions

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,42 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+//Memoizer 
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
 
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  }
+}
+
+
+//Recursive Solution (slow version)
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+fib = memoize(fib);
+ 
 module.exports = fib;
+
+//Iterative Solution
+// function fib(n) {
+//   let array = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     array.push(array[i - 2] + array[i - 1]);
+//   }
+
+//   return array[n];
+// }
