a15203a2201663f589c0a673b80b4701914bde1a
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-07-24 19:35:12 -0700

    implemented most linkedList methods

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,115 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let count = 0;
+    let node = this.head;
+    while(node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+    while(node.next) {
+      node = node.next;
+    }
+    return node;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) return;
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    if(!this.head) return;
+    if(!this.head.next) {
+      this.clear();
+      return;
+    }
+
+    let node = this.head;
+    let previous;
+
+    while(node.next) {
+      previous = node;
+      node = node.next;
+    }
+
+    previous.next = null;
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+
+    if (last) {
+      // There are some existing nodes in our chain
+      last.next = new Node(data);
+    } else {
+      // The chain is empty!
+      this.head = new Node(data);
+    }
+  }
+
+  getAt(index) {
+    let count = 0;
+    let node = this.head;
+
+    while(node) {
+      if (index === count) {
+        return node;
+      }
+      count++;
+      node = node.next;
+    }
+    return null;
+  }
+
+  removeAt(index) {
+    let count = 0;
+    let node = this.head;
+    let previous = this.getAt(index - 1);
+
+    if(index === 0) {
+      this.head = this.getAt(1);
+      return;
+    }
+
+    if (!previous || !previous.next) return;
+    previous.next = previous.next.next; 
+  }
+
+ }
 
 module.exports = { Node, LinkedList };
